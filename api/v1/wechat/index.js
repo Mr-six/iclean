000691ca3f5f1b2d7becc837391c6db8edb56cb5
Mr-six <582497915@qq.com>
2017-10-30 12:18:57 +0800

    add debug

--- api/v1/wechat/index.js
+++ api/v1/wechat/index.js
@@ -91,8 +91,11 @@ async function weCallBack (ctx) {
     sign,
   } = xml
 
+  $.info('微信回调', xml)
+
   if (return_code === 'SUCCESS' &&　result_code　=== 'SUCCESS') {
     let isRs = checkSign(xml)  // 签名验证
+    $.info('返回微信数据:', isRs)
     ctx.type = 'xml'
     ctx.body = isRs
     let query = {
@@ -104,6 +107,7 @@ async function weCallBack (ctx) {
     }
     try {
       let updata = await orderModel.update(query, info)
+      $.debug('微信订单更新', updata)
     } catch (e) {
       $.error(e)
     }
